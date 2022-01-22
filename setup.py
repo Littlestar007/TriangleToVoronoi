7c5034daf130da52de62988a300f45d22ba6fad0
LittleStar <1149556088@qq.com>
2022-01-22 14:06:36 +0800

    修改了文件

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 from setuptools import find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 VERSION = '0.1.0'
 
@@ -10,4 +12,11 @@ setup(
     description='三角形网格转为泰森多边形',  # package description
     packages=find_packages(),
     zip_safe=False,
+    url="https://github.com/Littlestar007/TriangleToVoronoi",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
 )
